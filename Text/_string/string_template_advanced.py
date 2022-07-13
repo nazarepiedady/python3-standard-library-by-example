a8db222d356130cb9e5ecb563251da641f9c532a
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-13 17:00:16 +0100

    chore: create the `template_text` variable with a template string

--- Text/_string/string_template_advanced.py
+++ Text/_string/string_template_advanced.py
@@ -4,3 +4,10 @@ import string
 class MyTemplate(string.Template):
     delimiter = '%'
     idpattern = '[a-z]+_[a-z]+'
+
+
+template_text = '''
+  Delimiter : %%
+  Replaced  : %with_underscore
+  Ignored   : %notunderscored
+'''
